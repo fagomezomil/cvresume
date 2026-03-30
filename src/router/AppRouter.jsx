6076caab09ad3373f3f6216967c2e5f49a0b8874
Federico Alvarez <fagomezomil@gmail.com>
2026-03-30 14:02:36 -0300

    ajuste dashboard para vercel

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import DashBoard from "../pages/dashboard"
+import DashBoard from "../pages/Dashboard"
 import Home from "../pages/Home"
 const AppRouter = () => {
-   
+
 
     return (
         <BrowserRouter >
             {/* <Layout> */}
-                <Routes>
+            <Routes>
 
-                    <Route path="/" element={<Home />} />
-                    <Route path="dashboard" element={<DashBoard />} />
-                    {/* <Route path="*" element={<Home />} /> */}
-                    
-                    
-                </Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="dashboard" element={<DashBoard />} />
+                {/* <Route path="*" element={<Home />} /> */}
+
+
+            </Routes>
             {/* </Layout> */}
         </BrowserRouter>
     )
